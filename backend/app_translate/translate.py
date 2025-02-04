e5e37897f22817a918072798831c048d175b2237
xieyan0811 <xieyan0811@sina.com>
2025-02-04 13:31:43 +0000

    adjust logic

--- backend/app_translate/translate.py
+++ backend/app_translate/translate.py
@@ -10,56 +10,48 @@ from loguru import logger
 from django.utils.translation import gettext as _
 from backend.common.user.user import *
 from backend.common.llm.llm_hub import llm_query, llm_query_json
+import simplemma
 
 from . import localdict
 from .models import StoreTranslate
+from .word_processor import ItemInfo, ItemOpt, WordManager, ItemWord
 
 TRANS_DEFAULT = "NOT FOUND"
 MSG_ROLE = _("you_are_a_middle_school_english_teacher")
 
 
-def find_word_local(word):
-    translation, rank, root = localdict.FreqTools.get_instance().lookfor(word)
-    if translation is not None:
-        return True, root, rank, translation
-    obj = StoreTranslate.objects.filter(word=word).first()
-    if obj:
-        if "regular_word" in obj.info:
-            return True, obj.info["regular_word"], obj.freq, obj.info["translate"]
-        else:
-            return True, obj.info["word"], localdict.DEFAULT_FREQ, obj.info["translate"]
-    return False, None, localdict.DEFAULT_FREQ, None
-
-
-def add_to_db(word, regular_word, dst, user_id, sentence=None, freq = localdict.DEFAULT_FREQ, wfrom = "USER", debug=False):
+def add_to_db(user_id, word, freq = localdict.DEFAULT_FREQ, wfrom = "USER", 
+                    word_info = None, debug=False):
+    """
+    support add and update
+    """
     if debug:
-        logger.info("now add_to_db inner", sentence)
+        logger.info("now add_to_db inner")
     now = datetime.datetime.now()
     timestr = now.strftime("%Y-%m-%d %H:%M:%S")
-    if freq == localdict.DEFAULT_FREQ:
-        freq = localdict.FreqTools.get_instance().get_freq(regular_word)
-    info = {"word": word, "regular_word": regular_word, "translate": dst, "freq": str(freq)}
-    if sentence is not None:
-        arr = sentence.split(" ")
-        if len(arr) > 3:
-            info["sentence"] = sentence
     if debug:
         logger.debug(f"info {info}")
-
-    db_word = regular_word
-    if StoreTranslate.objects.filter(word=db_word, user_id=user_id).exists():
-        obj = StoreTranslate.objects.filter(word=db_word, user_id=user_id).first()
+    new_info = ItemInfo(word_info, None)
+    
+    if StoreTranslate.objects.filter(word=word, user_id=user_id).exists():
+        obj = StoreTranslate.objects.filter(word=word, user_id=user_id).first()
         obj.times = obj.times + 1
-        obj.info = info
-        obj.wfrom = wfrom
+        if wfrom is not None:
+            obj.wfrom = wfrom
+        info = ItemInfo.deserialize(obj.info)
+        if info is not None:
+            info.update(new_info)
+            obj.info = info.serialize()
         obj.save()
     else:
+        if wfrom is None:
+            wfrom = "USER"
+        info = ItemInfo(word_info, ItemOpt())
         obj = StoreTranslate.objects.create(
-            word=db_word, info=info, freq=freq, user_id=user_id, times=1, wfrom=wfrom,
+            user_id=user_id, word=word, freq=freq, times=1, wfrom=wfrom, info=info.serialize(), 
             created_time=timestr
         )
     return True, obj
-
 
 def translate_sentence(user_id, sentence):
     content = "Please translate the following sentence to Chinese: {sentence}".format(
@@ -120,40 +112,61 @@ class TranslateWord:
         if debug:
             logger.debug(f"translate {word}, with_gpt {with_gpt}")
 
-        ret, en_regular, rank, translation = self.get_word_info(word, with_gpt, user_id, debug)
-        if ret:
-            r, obj = add_to_db(word, en_regular, translation, user_id, sentence=sentence, freq = rank)
-            if r:
-                if 'regular_word' in obj.info:
-                    src = obj.info["regular_word"]
-                else:
-                    src = obj.info["word"]
-                translation = _(
-                    "word_colon__{src}_enter_translation_colon__{dst}_enter_freq_colon__{freq}_enter_count_colon__{times}"
-                ).format(
-                    src=src,
-                    dst=obj.info["translate"],
-                    freq=obj.info["freq"],
-                    times=obj.times,
-                )
-        return ret, en_regular, translation
+        item_word = self.get_word_info(word, with_gpt, user_id, debug)
+        if item_word is None:
+            return False, None, None
+        if sentence is not None:
+            found = False
+            if item_word.example_list is None:
+                item_word.example_list = []
+            for example in item_word.example_list:
+                if example['sentence'] == sentence:
+                    found = True
+                    break
+            if not found:
+                ret, example = parse_sentence_example(user_id, word, sentence, debug=debug)
+                if ret:
+                    item_word.example_list.append(example)
+        ret, obj = add_to_db(user_id, item_word.word, item_word.freq, None, 
+                        item_word, debug=debug)
+        translation = _(
+            "word_{src}_translation_{dst}_phonetic_{phonetic}_freq_{freq}_count_{times}"
+        ).format(
+            src=item_word.word,
+            dst=item_word.get_meaning(),
+            phonetic=item_word.phonetic,
+            freq=item_word.freq,
+            times=obj.times,
+        )
+        return ret, item_word.word, translation
 
-    def get_word_info(self, word, with_gpt, user_id, debug):
-        ret, en_regular, rank, translation = find_word_local(word)
-        if ret:
-            logger.debug("found in local")
-        else:
+    def get_word_info(self, word, with_gpt, user_id, debug):        
+        wm = WordManager.get_instance()
+        word_item = wm.get_word(word)
+        if word_item is None:
+            lemmatized_text = simplemma.lemmatize(word, lang='en')
+            if lemmatized_text != word:
+                word_item = wm.get_word(lemmatized_text)
+                if word_item is not None:
+                    word = lemmatized_text
+        if word_item is None:
             if with_gpt:
-                ret, en_regular, translation = translate_word_gpt(
+                ret, en_regular, translation, phonetic = translate_word_gpt(
                     word, user_id, debug=debug
                 )
             else:
                 translation = self.translator.translate(word)
+                phonetic = None
                 if translation != "" and translation != word:
                     ret, en_regular, translation = True, word, translation
                 else:
                     ret, en_regular, translation = False, None, None
-        return ret, en_regular, rank, translation
+            if ret:
+                word_item = ItemWord(
+                    en_regular, localdict.DEFAULT_FREQ, phonetic=phonetic, wfrom='USER', meaning=translation
+                )
+        return word_item
+
 
 def translate_word(word, uid, with_gpt=True, sentence=None):
     return TranslateWord.get_instance().translate_word(
@@ -161,8 +174,8 @@ def translate_word(word, uid, with_gpt=True, sentence=None):
     )
 
 
-def translate_word_gpt(self, word, user_id, debug=False):
-    demo = "{'en_regular':'xxx', 'zh_main':'yyy', 'zh_all':'zzz'}"
+def translate_word_gpt(word, user_id, debug=False):
+    demo = "{'en_regular':'xxx', 'zh_main':'yyy', 'zh_all':'zzz', 'phonetic':'[aaa]'}"
     req = f"""
     Please take "{word}" and obtain its base form: when extracting the base form, 
     the part of speech remains unchanged, but plural forms, tenses, etc., are removed. 
@@ -178,22 +191,37 @@ def translate_word_gpt(self, word, user_id, debug=False):
         debug=False,
     )
     if ret:
-        if dic is not None and "en_regular" in dic and "zh_main" in dic and 'zh_all' in dic:
-            return ret, dic["en_regular"], dic["zh_all"]
-    return False, None, None
+        if dic is not None and "en_regular" in dic and "zh_main" in dic and 'zh_all' in dic and 'phonetic' in dic:
+            return ret, dic["en_regular"], dic["zh_all"], dic['phonetic']
+    return False, None, None, None
 
 
-def generate_sentence_example(user_id, word):
+def generate_sentence_example(user_id, word, debug=False):
     demo = '{"sentence": "This is a book.", "sentence_meaning": "这是一本书。", "word_meaning": "书"}'
     query = "Please give me a simple sentence with the word '{word}' in it, and translate the sentence to Chinese. result like {demo}".format(
         word=word, demo=demo
     )
     try:
         ret, dic, detail = llm_query_json(
-            user_id, MSG_ROLE, query, "translate", debug=True
+            user_id, MSG_ROLE, query, "translate", debug=debug
         )
         if ret and dic is not None and "sentence" in dic and "sentence_meaning" in dic and "word_meaning" in dic:
             return True, dic
     except Exception as e:
         logger.warning(f"generate_sentence_example error: {e}")
+    return False, {}
+
+def parse_sentence_example(user_id, word, sentence, debug=False):
+    demo = '{"sentence": "This is a book.", "sentence_meaning": "这是一本书。", "word_meaning": "书"}'
+    query = "Please parse the sentence '{sentence}' and extract the meaning of the word '{word}' in it. result like {demo}".format(
+        word=word, sentence=sentence, demo=demo
+    )
+    try:
+        ret, dic, detail = llm_query_json(
+            user_id, MSG_ROLE, query, "translate", debug=debug
+        )
+        if ret and dic is not None and "sentence" in dic and "sentence_meaning" in dic and "word_meaning" in dic:
+            return True, dic
+    except Exception as e:
+        logger.warning(f"parse_sentence_example error: {e}")
     return False, {}
