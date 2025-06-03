f8739710d0b4d01f5ba80c1a7e38d392c3e6346c
xieyan0811 <xieyan0811@sina.com>
2025-06-03 21:41:01 +0800

    update shell

--- shell/dev.sh
+++ shell/dev.sh
@@ -1,2 +1,3 @@
-docker compose --env-file backend/.env --profile development stop
+docker compose --env-file backend/.env --profile production down
+sleep 5
 docker compose --env-file backend/.env --profile development up -d
