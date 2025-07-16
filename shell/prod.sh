a512df73e18c323a6a5caf40f170b236fec8fe37
xieyan0811 <xieyan0811@sina.com>
2025-07-16 07:56:07 +0000

    update shell

--- shell/prod.sh
+++ shell/prod.sh
@@ -1,2 +1,4 @@
 docker compose --env-file backend/.env --profile production down
+docker compose --env-file backend/.env --profile development down
+sleep 5
 docker compose --env-file backend/.env --profile production up -d
